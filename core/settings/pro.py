629048c3927ea2c9d83b63f75961806eb40c972c
rahim <rahim.aghareb@gmail.com>
2024-08-23 10:32:07 +0330

    initail commit

---
+++ core/settings/pro.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['inex-design.com']
+
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASS,
+        'HOST': 'localhost',
+        'PORT': DB_PORT,
+    }
+}

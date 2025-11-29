58ca0f43babe802b673afe9d4a9dff8c42ad708b
rahim <rahim.aghareb@gmail.com>
2025-11-29 13:27:42 +0330

    add project category

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
+    path('categories/<str:category_slug>/', views.ProjectsListView.as_view(), name="projects_list_category"),
     path('<slug:slug>/', views.ProjectDetailsView.as_view(), name="project_details"),
     path('', views.ProjectsListView.as_view(), name="projects_list"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.utils.translation import gettext_lazy as _
 
-from .models import Project
+from .models import Project, Category
 
 
 class ProjectsListView(ListView):
@@ -11,9 +11,22 @@ class ProjectsListView(ListView):
     context_object_name = 'projects'
     template_name = 'projects/projects_list.html'
     
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        category_slug = self.kwargs.get('category_slug', '')
+        if category_slug:
+            try:
+                category = Category.objects.get(slug=category_slug)
+                queryset = queryset.filter(category=category)
+            except:
+                pass
+        return queryset
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page_title"] = _("Projects")
+        context["category"] = self.kwargs.get('category_slug', '')
+        context["categories"] = Category.objects.all()
         return context
         
 

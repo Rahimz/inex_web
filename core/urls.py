a2d2ca10a5c33c83a0e5212dc0d93a8abf04f01f
rahim <rahim.aghareb@gmail.com>
2025-12-27 11:41:48 +0330

    update robots.txt

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic.base import TemplateView
 
 urlpatterns = i18n_patterns(
     path('projects/', include('projects.urls')),
@@ -26,6 +27,7 @@ urlpatterns = i18n_patterns(
     path('analytics/', include('analytics.urls')),
     path('rosetta/', include('rosetta.urls')),
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', include('generals.urls')),
 )
